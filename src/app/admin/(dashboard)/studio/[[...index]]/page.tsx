8867206644e6d788eaac90edfb6ec3fd98fff197
unknown <huznigarane@gmail.com>
2026-05-23 08:20:33 +0300

    Fix Studio wrapper height

--- src/app/admin/(dashboard)/studio/[[...index]]/page.tsx
+++ src/app/admin/(dashboard)/studio/[[...index]]/page.tsx
@@ -5,7 +5,7 @@ import config from "../../../../../../sanity.config";
 
 export default function StudioPage() {
   return (
-    <div className="h-[80vh] w-full bg-white rounded-lg overflow-hidden shadow border border-gray-100">
+    <div className="h-[calc(100vh-120px)] w-full bg-white rounded-lg shadow border border-gray-100 relative">
       <NextStudio config={config} />
     </div>
   );
